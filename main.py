2779bed978a24020875f9fd8d255acfd646f3d1f
LeonidKhoussid <leoforrussia@gmail.com>
2025-03-30 17:39:21 +0300

    Initial commit 4

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
-import os
 
 app = FastAPI()
 
-# Allow all origins (CORS)
+# Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or your frontend domain
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 @app.get("/posts")
-def get_posts():
+def read_csv():
     try:
         df = pd.read_csv("transformed_data.csv")
         return df.fillna("").to_dict(orient="records")
     except Exception as e:
         return {"error": str(e)}
-
-# Main entry point for Render
-if __name__ == "__main__":
-    import uvicorn
-
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
